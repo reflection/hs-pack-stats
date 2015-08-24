782dac75c50e7087699c6a0ba2efa7541eead22f
Samuel Chou <sam.chou@windystudios.com>
2015-08-24 10:49:39 -0700

    Prettify stats output and attempt Windows compatibility

--- hs_pack_stats.py
+++ hs_pack_stats.py
@@ -1,9 +1,9 @@
-#!/usr/local/bin/python
 import json
 import os
 import re
 import sys
 import time
+from collections import OrderedDict
 
 """ Hearthstone Pack Statistics Tracker """
 
@@ -11,14 +11,20 @@ import time
 stats_file = os.path.expanduser('~/Documents/hs_pack_stats.txt')
 hs_log_dir = os.path.expanduser('~/Library/Preferences/Blizzard/Hearthstone/Logs')
 
-# UNTESTED WINDOWS PATHS
-# stats_file = os.path.expanduser('~/Documents/hs_pack_stats.txt')
-# hs_log_dir = os.path.expanduser('~/Library/Preferences/Blizzard/Hearthstone/Logs')
-
 if not os.path.exists(hs_log_dir):
     sys.exit('Error: Hearthstone log dir does not exist: ' + hs_log_dir)
 
-pack_re = re.compile('.+\[Achievements\] NotifyOfCardGained: \[name=(.+?) cardId=(.+?) type=.+Premium=(.+?)\] (.+)')
+# By default only read latest log file
+log = sorted(
+    [os.path.join(hs_log_dir, f) for f in os.listdir(hs_log_dir)],
+    key=lambda l: os.path.getmtime(l),
+    reverse=True
+)[0]
+
+# Windows Paths (untested)
+# IMPORTANT: Comment out all lines above this
+# stats_file = os.path.expanduser('~\\My Documents\\hs_pack_stats.txt')
+# log = os.path.expanduser('~\\AppData\\Local\\Blizzard\\Hearthstone\\Logs\\Power.log')
 
 # Re-organize Hearthstone json data by ids
 hs_data = {}
@@ -28,25 +34,19 @@ with open('AllSets.json', 'r') as f:
         for d in data[key]:
             hs_data[d.pop('id')] = d
 
-# By default only read latest log file
-log = sorted(
-    [os.path.join(hs_log_dir, f) for f in os.listdir(hs_log_dir)],
-    key=lambda l: os.path.getmtime(l),
-    reverse=True
-)[0]
-
-stats = {
-    'playerClass': { },
-    'rarity':      { },
-    'type':        { },
-    'bling':       {
+pack_re = re.compile('.+\[Achievements\] NotifyOfCardGained: \[name=(.+?) cardId=(.+?) type=.+Premium=(.+?)\] (.+)')
+stats = OrderedDict([
+    ('rarity',      {}),
+    ('playerClass', {}),
+    ('type',        {}),
+    ('race',        {}),
+    ('bling',       {
         'plain':  0,
         'golden': 0,
-    },
-    'race':        { },
-    'dups':  0,
-    'total': 0,
-}
+    }),
+    ('dups',        0),
+    ('total',       0)
+])
 
 def update_hs_stats(card_id):
     for k in stats.keys():
@@ -60,12 +60,35 @@ def update_hs_stats(card_id):
                 stats[k][v] = 0
             stats[k][v] += 1
 
+def format_stats(stats, fs, total, level=0):
+    length = 14
+    for k in stats:
+        if k == 'playerClass':
+            name = 'Class'
+        else:
+            name = k.capitalize()
+        if isinstance(stats[k], (int, long)):
+            indent = 2 * level
+            pad = length - len(name) - indent
+            pct = 0
+            if total > 0:
+                pct = (float(stats[k]) / total) * 100
+            fs.append(' ' * indent + '{name}{v: {pad}} ({pct:.2f}%)'.format(
+                name=name, v=stats[k], pad=pad, pct=pct
+            ))
+        else:
+            fs.append(name)
+            format_stats(stats[k], fs, total, level+1)
+        if level == 0:
+            fs.append('')
+    return fs
+
 # Update stats file when hs log file is updated
-mod_ts = 0
+file_mod = 0
 file_pos = -1
 while True:
-    if os.path.getmtime(log) > mod_ts:
-        mod_ts = os.path.getmtime(log)
+    if os.path.getmtime(log) > file_mod:
+        file_mod = os.path.getmtime(log)
 
         with open(log, 'r') as f:
             if file_pos > 0:
@@ -99,7 +122,10 @@ while True:
                     stats['total'] += 1
             file_pos = f.tell()
 
+        fs = []
+        fs = '\n'.join(format_stats(stats, fs, stats['total']))
+        print fs
         with open(stats_file, 'w') as f:
-            json.dump(stats, f, indent=2)
+            f.write(fs)
 
     time.sleep(1)
